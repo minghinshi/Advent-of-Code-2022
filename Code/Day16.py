09d87cc9cb8333a30d2dac1d187272f4ff6d9d77
minghinshi <mhshi9768@gmail.com>
2022-12-25 18:13:00 +0800

    Day 16 Part 2
    Aww yeah

--- Code/Day16.py
+++ Code/Day16.py
@@ -1,75 +1,80 @@
+import copy
+
 class player:
-    def __init__(self, location, time_left) -> None:
-        self.location = location
-        self.time_left = time_left
+    def __init__(self) -> None:
+        self.location = "AA"
+        self.time_to_spend = 0
+
+    def move_to_valve(self, valve):
+        self.time_to_spend -= compressed_graph[self.location][valve]
+        self.location = valve
+
+    def get_choices(self, opened_valves):
+        choices = set()
+        for valve in get_target_valves():
+            if valve not in opened_valves and compressed_graph[self.location][valve] <= self.time_to_spend:
+                choices.add(valve)
+        return choices
+
+    def __str__(self) -> str:
+        return f"Player at {self.location} with {self.time_to_spend} minutes to spend."
 
 class node:
-    def __init__(self, locations, opened_valves, time_lefts, pressure) -> None:
-        self.locations: list[str] = locations
-        self.time_lefts: list[int] = time_lefts
-        self.pressure: int = pressure
-        self.opened_valves: set[str] = opened_valves
+    def __init__(self, player_count, moves_allowed) -> None:
+        self.players = [player() for _ in range(player_count)]
+        self.moves_left = moves_allowed
+        self.opened_valves = set()
+        self.pressure = 0
+        self.turn = 0
 
-    def get_flow_rate(self):
-        return flow_rates[self.location]
+    def open_valve(self, valve):
+        self.get_current_player().move_to_valve(valve)
+        self.opened_valves.add(valve)
+        self.pressure += (self.moves_left - 1) * flow_rates[valve]
 
-    def can_open_valve(self, valve, player):
-        return valve not in self.opened_valves and compressed_graph[self.locations[player]][valve] < self.time_left
+    def elapse_turn(self):
+        self.turn += 1
+        if self.turn == len(self.players):
+            self.turn = 0
+            self.moves_left -= 1
 
-    def open_valve(self, valve, player):
-        new_locations = self.locations.copy()
-        new_locations[player] = valve
-        new_time_left = self.time_lefts.copy()
-        new_time_left[player] -= compressed_graph[self.locations[player]][valve]
-        new_pressure = self.pressure + flow_rates[valve] * new_time_left[player]
-        new_opened_valves = self.opened_valves.copy()
-        new_opened_valves.add(valve)
-        return node(new_locations, new_opened_valves, new_time_left, new_pressure)
+    def get_current_player(self) -> player:
+        return self.players[self.turn]
 
-    def get_neighbours(self):
-        return {self.open_valve(valve) for valve in filter(self.can_open_valve, valves)}
+    def get_children(self):
+        children = set()
 
-    def get_key(self):
-        return self.location, tuple(self.opened_valves)
+        # First, the player on this round gets 1 time to spend:
+        player = self.get_current_player()
+        player.time_to_spend += 1
 
-    def print(self):
-        print(self.location, self.opened_valves, self.time_left, self.pressure)
+        # Then, the player thinks about where to spend their time:
+        for choice in player.get_choices(self.opened_valves):
+            child = copy.deepcopy(self)
+            child.open_valve(choice)
+            child.elapse_turn()
+            children.add(child)
 
-def sign(x):
-    return -1 if x < 0 else 0 if x == 0 else 1
+        # The player can also choose to do nothing
+        child = copy.deepcopy(self)
+        child.elapse_turn()
+        children.add(child)
+      
+        return children
+
+    def __str__(self) -> str:
+        string = ""
+        for player in self.players:
+            string += f"{player}\n"
+        string += f"Opened valves: {self.opened_valves}. {self.pressure} pressure released.\n"
+        string += f"It is currently Player {self.turn+1}'s turn. {self.moves_left} minutes left.\n"
+        return string
 
 def parse_line(strings: str):
     name = strings[1]
     flow_rates[name] = int(strings[4][5:-1])
     graph[name] = [string.removesuffix(",") for string in strings[9:]]
 
-def get_max_pressure(root: node):
-    nodes = {root.get_key(): [root]}
-    unvisited = [root]
-    while len(unvisited) != 0:
-        current_node = unvisited.pop(0)
-        current_node.print()
-        for node in current_node.get_neighbours():
-            add_to_queue = True
-            for other in nodes.setdefault(node.get_key(), []):
-                if not node.any_better_than(other):
-                    add_to_queue = False
-                if node.both_better_than(other):                   
-                    nodes[node.get_key()].remove(other)
-                    if other in unvisited:
-                        unvisited.remove(other)
-            if add_to_queue:
-                nodes[node.get_key()].append(node)
-                unvisited.append(node)
-    return max(max(node.pressure for node in list) for list in nodes.values())
-
-def get_max_pressure_greedy(root: node):
-    current = root
-    while True:
-        neighbours = current.get_neighbours()
-        if len(neighbours) == 0:
-            return current.pressure
-        current = sorted(neighbours, key=lambda x: x.pressure)[-1]
         
 def get_shortest_paths(root, targets):
     costs = {root: 0}
@@ -92,6 +97,18 @@ def compress_graph():
     compressed_graph["AA"] = get_shortest_paths("AA", valves)
     return compressed_graph
 
+def get_highest_pressure(player_count, moves_allowed):
+    nodes = [node(player_count, moves_allowed)]
+    for i in range(player_count * moves_allowed):
+        print(f"Iterations = {i+1}, Nodes = {len(nodes)}")
+        children: list[node] = []
+        for current in nodes:
+            children.extend(current.get_children())
+        children.sort(key=lambda x: x.pressure)
+        children.reverse()
+        nodes = children[:1000]
+    return max(map(lambda x: x.pressure, nodes))
+            
 flow_rates = {}
 graph = {}
 
@@ -101,5 +118,5 @@ with open("Inputs/Day16.txt") as file:
 
 valves = get_target_valves()
 compressed_graph = compress_graph()
-
-print(get_max_pressure(node("AA", set(), 30, 0)))
+print(get_highest_pressure(1, 30))
+print(get_highest_pressure(2, 26))
